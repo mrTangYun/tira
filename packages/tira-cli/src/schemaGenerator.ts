2367c52b0d55700b6a3d2bb36def1d533c629b10
王伟 <wangwei@speedto.cn>
2018-09-17 12:36:14 +0800

    fix schema.config.js file error

--- packages/tira-cli/src/schemaGenerator.ts
+++ packages/tira-cli/src/schemaGenerator.ts
@@ -1,9 +1,10 @@
 import * as path from 'path';
 import { generateJsonFileByUrl } from '@tira/tira-graphql';
 
-const schemaConfig = require(path.resolve(process.cwd(), 'schema.config.js'));
+const schemaConfigFile = 'schema.config.js';
 
 export async function generateFrontendSchema() {
+  const schemaConfig = require(path.resolve(process.cwd(), schemaConfigFile));
   if (schemaConfig.frontend) {
     const generatePath = path.resolve(process.cwd(), schemaConfig.frontend.schemaPath);
     await generateJsonFileByUrl(schemaConfig.frontend.remoteUrl, generatePath);
@@ -11,6 +12,7 @@ export async function generateFrontendSchema() {
 }
 
 export async function generateBackendSchema() {
+  const schemaConfig = require(path.resolve(process.cwd(), schemaConfigFile));
   if (schemaConfig.backend) {
     const generatePath = path.resolve(process.cwd(), schemaConfig.backend.schemaPath);
     await generateJsonFileByUrl(schemaConfig.backend.remoteUrl, generatePath);
